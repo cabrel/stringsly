43610156975768a1aeaf35ca7eabe6b0bc091851
Robin Harper <cabrel@zerklabs.com>
2014-02-26 00:45:00 -0500

    Updated to write each type to its own file

---
+++ README.md
@@ -0,0 +1,9 @@
+stringsly
+=========
+
+Simplifies running platform-dependent strings utilities, including all encodings on unix systems.
+
+For windows, the Sysinternals Strings program is required: [Sysinternals Strings][winstrings]
+
+
+[winstrings]: http://technet.microsoft.com/en-us/sysinternals/bb897439
--- stringsly.go
+++ stringsly.go
@@ -7,57 +7,32 @@ import (
 	"log"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 )
 
-var res []byte
-
-func runStrings(filePath string, opts string) {
-	argv := []string{
-		"-a",
-		"-td",
-		opts,
-		filePath,
-	}
-
-	cmd := exec.Command("strings", argv...)
-	lres, err := cmd.CombinedOutput()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var header []byte
-
-	if opts == "-el" {
-		header = []byte("------ 16-bit (strings -el) ------\n")
-	} else if opts == "-eL" {
-		header = []byte("------ 32-bit (strings -eL) ------\n")
-	} else if opts == "-eB" {
-		header = []byte("------ 32-bit (strings -eB) ------\n")
-	} else if opts == "-eb" {
-		header = []byte("------ 16-bit (strings -eb) ------\n")
-	} else if opts == "-eS" {
-		header = []byte("------ 8-bit (strings -eS) ------\n")
-	} else if opts == "-es" {
-		header = []byte("------ 7-bit (strings -es) ------\n")
-	}
-
-	res = combine(res, combine(header, lres))
-}
+var (
+	fileToString     = flag.String("file", "", "File to generate strings for")
+	includeLocations = flag.Bool("locations", false, "Include the location of the strings. Defaults to -td")
+	currentOS        string
+)
 
 func main() {
 	flag.Parse()
-	fileToString := flag.Arg(0)
 
-	runStrings(fileToString, "-eL")
-	runStrings(fileToString, "-el")
-	runStrings(fileToString, "-eB")
-	runStrings(fileToString, "-eb")
-	runStrings(fileToString, "-eS")
-	runStrings(fileToString, "-es")
+	if len(*fileToString) == 0 {
+		log.Fatal("File must be provided")
+	}
 
-	_, fname := filepath.Split(fileToString)
-	ioutil.WriteFile(fmt.Sprintf("%s.str", fname), res, 0777)
+	if runtime.GOOS == "windows" {
+		runStrings(*fileToString, *includeLocations, "")
+	} else {
+		runStrings(*fileToString, *includeLocations, "-eL")
+		runStrings(*fileToString, *includeLocations, "-el")
+		runStrings(*fileToString, *includeLocations, "-eB")
+		runStrings(*fileToString, *includeLocations, "-eb")
+		runStrings(*fileToString, *includeLocations, "-eS")
+		runStrings(*fileToString, *includeLocations, "-es")
+	}
 }
 
 func combine(b1 []byte, b2 []byte) []byte {
@@ -66,4 +41,49 @@ func combine(b1 []byte, b2 []byte) []byte {
 	copy(newslice, b1)
 	copy(newslice[len(b1):], b2)
 	return newslice
+}
+
+func runStrings(filePath string, incLoc bool, opts string) {
+	var res []byte
+	var argv []string
+	var ext string
+
+	if runtime.GOOS == "windows" {
+		if incLoc {
+			argv = append(argv, "-o")
+		}
+	} else {
+		argv = append(argv, "-a")
+
+		if incLoc {
+			argv = append(argv, "-td")
+		}
+
+		argv = append(argv, opts)
+	}
+
+	argv = append(argv, filePath)
+
+	cmd := exec.Command("strings", argv...)
+	lres, _ := cmd.CombinedOutput()
+
+	if opts == "-el" {
+		ext = "16bitle"
+	} else if opts == "-eL" {
+		ext = "32bitle"
+	} else if opts == "-eB" {
+		ext = "32bitbe"
+	} else if opts == "-eb" {
+		ext = "32bitle"
+	} else if opts == "-eS" {
+		ext = "8bit"
+	} else if opts == "-es" {
+		ext = "7bit"
+	} else {
+		ext = "si"
+	}
+
+	res = combine(res, lres)
+	_, fname := filepath.Split(filePath)
+	ioutil.WriteFile(fmt.Sprintf("%s.str.%s", fname, ext), res, 0777)
 }
